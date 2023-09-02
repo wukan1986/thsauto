7b4ba01422a8649e69301ac63328e26e5020552b
伍侃 <wu-kan@163.com>
2023-09-02 21:47:31 +0800

    Update sell_more.py

--- examples/sell_more.py
+++ examples/sell_more.py
@@ -1,9 +1,10 @@
 from thsauto import THS
+from loguru import logger
 
 t = THS(debug=True, skip_popup=False)
 t.connect(addr="emulator-5554")
 
-code = 600700  # 600000 # 000001
+code = 600600  # 600000 # 000001
 price = float('nan')  # nan表示使用界面自动填充的对手价
 qty = 100
 
@@ -12,9 +13,9 @@ for r in range(10):
     code += 1
     price += 1
     qty += 2
-    print('=' * 20, code, price, qty)
+    logger.info('{}, {}, {}, {}', '=' * 20, code, price, qty)
     try:
         confirm, prompt = t.sell(qty, price=price, code=f'{code:06d}', debug=True)
-        print(confirm, prompt)
+        logger.info('{}, {}', confirm, prompt)
     except AssertionError as e:
-        print(e)
+        logger.info(e)
